15ca2f1c447bd29a191b8487d4b6b15c20101fe7
OneBigOwnage <niekvandenbos@hotmail.com>
2018-09-16 15:42:58 +0200

    Add drag-n-drop to the application.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import InitialState from './InitialState';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.todoList = React.createRef();
     this.state = InitialState;
   }
   getNewID = () => {
@@ -16,20 +17,21 @@ class App extends Component {
       return 0;
     }
 
-    return ids.sort().pop() + 1;
+    return (ids.sort((a, b) => a - b).pop()) + 1;
   }
   addTask = (task) => {
     task.id = this.getNewID();
     let tasks = Array.from(this.state.tasks);
-    tasks.push(task);
+    tasks.unshift(task);
 
     this.setState({ tasks: tasks });
+    this.todoList.current.addTask(task);
   }
   removeTask = (taskID) => {
     let tasks = Array.from(this.state.tasks);
     tasks = tasks.filter(task => task.id !== Number(taskID));
 
-    this.setState({tasks: tasks});
+    this.setState({ tasks: tasks });
   }
   render() {
     return (
@@ -37,7 +39,7 @@ class App extends Component {
         <nav id="tasks-panel" className="container">
           <Title text="My personal todo list" />
           <AddTask addTask={this.addTask} />
-          <TaskList tasks={this.state.tasks} removeTask={this.removeTask} />
+          <TaskList tasks={this.state.tasks} removeTask={this.removeTask} ref={this.todoList} />
         </nav>
       </div>
     );
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,15 +1,73 @@
 import React, { Component } from 'react';
 import Task from './Task';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 class TaskList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { tasks: props.tasks };
+        this.onDragEnd = this.onDragEnd.bind(this);
+    }
+    onDragEnd = (result) => {
+        if (!result.destination) {
+            return;
+        }
+        let tasks = this.reOrder(this.state.tasks, result.source.index, result.destination.index);
+        this.setState({ tasks: tasks });
+    }
+    reOrder = (list, startIndex, endIndex) => {
+        const result = Array.from(list);
+        const [removed] = result.splice(startIndex, 1);
+        result.splice(endIndex, 0, removed);
+
+        return result;
+    }
+    getItemStyle = (isDragging, draggableStyle) => {
+        let style = { ...draggableStyle };
+
+        if (isDragging) {
+            style.backgroundColor = '#FFFA70';
+            style.transform += 'rotate(-2deg)';
+        }
+
+        return style;
+    }
+    addTask = (task) => {
+        const tasks = Array.from(this.state.tasks);
+        tasks.unshift(task);
+
+        this.setState({ tasks: tasks });
+    }
     render() {
         return (
             <div className="task-list">
-                {
-                    this.props.tasks.map(task =>
-                        <Task key={task.id} task={task} taskID={task.id} removeTask={this.props.removeTask} />
-                    )
-                }
+
+                <DragDropContext onDragEnd={this.onDragEnd}>
+                    <Droppable droppableId="droppable">
+                        {(provided, snapshot) => (
+                            <div
+                                ref={provided.innerRef}>
+                                {this.state.tasks.map((task, index) =>
+                                    <Draggable key={task.id} draggableId={task.id} index={index}>
+                                        {(provided, snapshot) => (
+                                            <div
+                                                ref={provided.innerRef}
+                                                {...provided.draggableProps}
+                                                {...provided.dragHandleProps}
+                                                style={this.getItemStyle(
+                                                    snapshot.isDragging,
+                                                    provided.draggableProps.style
+                                                )}
+                                            >
+                                                <Task key={task.id} task={task} taskID={task.id} removeTask={this.props.removeTask} />
+                                            </div>
+                                        )}
+                                    </Draggable>
+                                )}
+                            </div>
+                        )}
+                    </Droppable>
+                </DragDropContext>
 
             </div>
         );
